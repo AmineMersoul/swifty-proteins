e62d9c1926e881f91cbc21cceb6a36b99cd3edca
amersoul <amine.3dsmax@live.fr>
2021-07-07 16:53:31 +0100

    auth with biometrics

--- swifty-proteins/ViewController.swift
+++ swifty-proteins/ViewController.swift
@@ -7,14 +7,49 @@
 //
 
 import UIKit
+import LocalAuthentication
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var authButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let context = LAContext()
+        var error: NSError? = nil
+        if (context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error)) {
+            print("Biometrics auth supported!")
+        } else {
+            authButton.isHidden = true
+            print("Biometrics auth not supported!")
+        }
     }
 
-
+    @IBAction func auth(_ sender: Any) {
+        let context = LAContext()
+        var error: NSError? = nil
+        if (context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error)) {
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: "Please Auth with touch id!") { [weak self]success, error in
+                DispatchQueue.main.async {
+                     guard success, error == nil else {
+                        // failed
+                        return
+                    }
+                    let alert = UIAlertController(title: "Matched", message: "your biometrics id is matched!", preferredStyle: UIAlertController.Style.alert)
+                    alert.addAction(UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: nil))
+                    self?.present(alert, animated: true)
+                    
+                    // success
+                }
+            }
+        } else {
+            // can't use
+            let alert = UIAlertController(title: "not supported", message: "your phone dont support biometrics", preferredStyle: UIAlertController.Style.alert)
+            alert.addAction(UIAlertAction(title: "Dismiss", style: UIAlertAction.Style.default, handler: nil))
+            present(alert, animated: true)
+        }
+    }
+    
 }
 
