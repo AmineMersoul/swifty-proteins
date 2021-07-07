1f879b70454c036031aa3610f0337ddbe9430a58
amersoul <amine.3dsmax@live.fr>
2021-07-07 12:43:18 +0100

    Initial Commit

---
+++ swifty-proteins/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  swifty-proteins
+//
+//  Created by Amine Mersoul on 7/7/21.
+//  Copyright © 2021 Amine Mersoul. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
